1966e76b20bbb14814507c2ddddbce0f9d705723
Mimi1973 <etse.haile2@gmail.com>
2022-08-31 22:00:47 -0400

    My commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'jb65wt',
+  projectId: "jb65wt",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
-//sk4qip'
